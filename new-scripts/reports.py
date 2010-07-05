7597b719086528227b9c4367f0f84da56ee4f2e8
Jendrik Seipp <jendrikseipp@web.de>
2010-07-05 23:59:26 +0000

    add filters, grouping and ordering

--- new-scripts/reports.py
+++ new-scripts/reports.py
@@ -33,6 +33,10 @@ class ReportOptionParser(OptionParser):
         self.add_option(
             "-d", "--dest", action="store", dest="report_dir", default="",
             help="path to report directory (default: <eval_dir>-report)")
+            
+        self.add_option(
+            "-r", "--run-filter", action="extend", dest="run_filter", default=[],
+            help="")
         
     def error(self, msg):
         '''Show the complete help AND the error message'''
@@ -54,6 +58,8 @@ class ReportOptionParser(OptionParser):
             dir_name = os.path.basename(options.eval_dir)
             options.report_dir = os.path.join(parent_dir, dir_name + '-report')
             logging.info('Report dir: "%s"' % options.report_dir)
+            
+        options.run_filter = []
         
         return options
         
@@ -72,8 +78,43 @@ class Report(object):
         
         self.data = self._get_data()
         
+        self.filter_funcs = []
+        self.filter_pairs = {}
+        
+        self.grouping = None
+        self.order = None
+        
+    def add_run_filter(self, *filter_funcs, **filter_pairs):
+        self.filter_funcs.extend(filter_funcs)
+        self.filter_pairs.update(filter_pairs)
+        
+    def set_grouping(self, *grouping):
+        self.grouping = grouping
+        
+    def set_order(self, *order):
+        self.order = order
+        
     def build(self):
-        raise Exception('Not Implemented')
+        #raise Exception('Not Implemented')
+        data = DataSet(self.data)
+        data.dump()
+        
+        if not self.order:
+            self.order = ['expanded']
+        data.sort(*self.order)
+        print 'SORTED'
+        data.dump()
+        
+        if self.filter_funcs or self.filter_pairs:
+            data = data.filtered(*self.filter_funcs, **self.filter_pairs)
+            print 'FILTERED'
+            data.dump()
+        if self.grouping:
+            groups = data.groups(*self.grouping)
+            print 'GROUPED'
+            for name, group in groups:
+                print group
+        
         
     def _get_data(self):
         data = DataSet()
@@ -81,18 +122,30 @@ class Report(object):
             for file in files:
                 if file == 'properties':
                     file = os.path.join(base, file)
-                    props = ConfigObj(file)
+                    print 'FILE', open(file).read()
+                    props = tools.Properties(file)
+                    print props
+                    print
                     data.append(**props)
         return data
     
 
 
 if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        sys.argv.extend(['-s', 'test-eval'])
     report = Report()
-    dataset = report.data
-    for _, group in report.data.groups('config', 'domain'):
-        group.dump()
-    print 
-    #report.build()
+    report.add_run_filter(domain='gripper')
+    #report.add_run_filter(lambda item: item['expanded'] == '21')
+    report.set_grouping('domain', 'config')
+    data = report.data
+    print 'ITEMS', data.items
+    #for _, group in data.groups('config', 'domain'):
+    #    group.dump()
+    #print
+    #for item in data.filtered(config='yY'):
+    #    print item
+    
+    report.build()
     
 
