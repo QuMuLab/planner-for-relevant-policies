a3a67d33732419e274ff88ae9d5368fac656cabc
Erez Karpas <karpase@gmail.com>
2010-08-18 13:16:02 +0000

    Don't run LAMA in the nightly test (it takes a long time anyway)

--- new-scripts/downward_configs_nightly.py
+++ new-scripts/downward_configs_nightly.py
@@ -21,16 +21,6 @@ fFyY = """\
 --search "lazy_greedy(hff, hcea, preferred=(hff, hcea))"\
 """
 
-lama = """\
---heuristic "hff=ff()" --heuristic "hlm=lmcount()" --search \
-"iterated(lazy_wastar(hff,hlm,preferred=(hff,hlm),w=10),\
-lazy_wastar(hff,hlm,preferred=(hff,hlm),w=5),\
-lazy_wastar(hff,hlm,preferred=(hff,hlm),w=3),\
-lazy_wastar(hff,hlm,preferred=(hff,hlm),w=2),\
-lazy_wastar(hff,hlm,preferred=(hff,hlm),w=1),\
-repeat_last=true)"\
-"""
-
 blind = """\
 --search "astar(blind())"\
 """
