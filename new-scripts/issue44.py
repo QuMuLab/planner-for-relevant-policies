8f2235693b13c8551b081062eb3c2c679d4f4075
Jendrik Seipp <jendrikseipp@web.de>
2010-11-12 13:56:19 +0100

    simplify issue44.py script

--- new-scripts/issue44.py
+++ new-scripts/issue44.py
@@ -5,12 +5,15 @@ from downward_experiments import *
 old_rev = 'a71e8f733599'
 new_rev = '952811b6757e'
 
-repo = '../'
+old_translator = TranslatorHgCheckout(rev=old_rev)
+new_translator = TranslatorHgCheckout(rev=new_rev)
+preprocessor = PreprocessorHgCheckout(rev=old_rev)
+planner = PlannerHgCheckout(rev=old_rev)
 
 
 combinations = [
-    (TranslatorHgCheckout(repo=repo, rev=old_rev), PreprocessorHgCheckout(repo=repo, rev=old_rev), PlannerHgCheckout(repo=repo, rev=old_rev)),
-    (TranslatorHgCheckout(repo=repo, rev=new_rev), PreprocessorHgCheckout(repo=repo, rev=old_rev), PlannerHgCheckout(repo=repo, rev=old_rev)),
+    (old_translator, preprocessor, planner),
+    (new_translator, preprocessor, planner),
                ]
                
 build_experiment(combinations)
