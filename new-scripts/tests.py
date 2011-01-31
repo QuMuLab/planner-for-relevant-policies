721833f7ec2ccab53a2bf1cf9ededc28d998af4e
Jendrik Seipp <jendrikseipp@web.de>
2011-01-31 13:59:26 +0100

    pep8

--- new-scripts/tests.py
+++ new-scripts/tests.py
@@ -4,7 +4,8 @@ import os
 import datetime
 import logging
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)-s %(levelname)-8s %(message)s',)
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)-s %(levelname)-8s %(message)s',)
 
 from tools import copy, prod
 from reports import gm
@@ -26,32 +27,38 @@ os.mkdir(src_dir)
 open(nested_src_file, 'w').close()
 os.mkdir(dest_dir1)
 
+
 def test_copy_file_to_file():
     copy(src_file, dest_file)
     assert os.path.isfile(os.path.join(base, 'dest1', 'dest_file'))
+
 
 def test_copy_file_to_ex_dir():
     copy(src_file, dest_dir1)
     assert os.path.isfile(os.path.join(base, 'dest_dir_existing', 'src_file'))
 
+
 def test_copy_file_to_not_ex_dir():
     copy(src_file, dest_dir2)
     assert os.path.isfile(os.path.join(base, 'dest_dir_not_existing'))
+
 
 def test_copy_dir_to_dir():
     copy(src_dir, dest_dir3)
     assert os.path.isdir(os.path.join(base, 'dest_dir_also_not_existing'))
     assert os.path.isfile(os.path.join(base, 'dest_dir_also_not_existing',
-                                        'nested_src_file'))
+                                       'nested_src_file'))
 
 
 def gm_old(values):
-    return round(prod(values) ** (1/len(values)), 4)
+    return round(prod(values) ** (1 / len(values)), 4)
+
 
 def test_gm1():
     lists = [1, 2, 4, 5], [0.4, 0.8], [2, 8], [10 ** (-5), 5000]
     for l in lists:
         assert gm_old(l) == gm(l)
+
 
 if __name__ == '__main__':
     test_copy_file_to_file()
