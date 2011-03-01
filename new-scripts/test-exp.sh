3d7ba76428a959c811d4eb3f2911f56fd6d4a7ea
Jendrik Seipp <jendrikseipp@web.de>
2011-03-01 23:09:36 +0100

    make testsuite smaller

--- new-scripts/test-exp.sh
+++ new-scripts/test-exp.sh
@@ -5,7 +5,7 @@ EXPNAME=test-exp
 CONFIGS=downward_configs.py:yY
 QUEUE=athlon_core.q
 
-SUITE=MINITEST
+SUITE=gripper:prob01.pddl,zenotravel:pfile1
 EXPTYPE=local
 
 # We only want a quick look if everything worked
