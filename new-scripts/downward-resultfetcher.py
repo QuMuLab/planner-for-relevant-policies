800c0f7de36d1ee266e448f7843f2b213bf5c182
Jendrik Seipp <jendrikseipp@web.de>
2011-03-04 19:44:21 +0100

    parse number of dead-end states from logs

--- new-scripts/downward-resultfetcher.py
+++ new-scripts/downward-resultfetcher.py
@@ -393,6 +393,8 @@ def add_search_parsing(eval):
     eval.add_pattern('memory', r'Peak memory: (.+) KB', type=int,
                      required=False)
     eval.add_pattern('cost', r'Plan cost: (.+)', type=int, required=False)
+    eval.add_pattern('dead_ends', r'Dead ends: (.+) state\(s\)\.', type=int,
+                     required=False)
 
 
 def add_search_functions(eval):
