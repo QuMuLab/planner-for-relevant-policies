b08138e8adb4cd552abcecf611a4b4b5a855739c
Jendrik Seipp <jendrikseipp@web.de>
2011-05-10 17:09:24 +0200

    adapt test-exp.sh for tests on grid

--- new-scripts/test-scripts.sh
+++ new-scripts/test-scripts.sh
@@ -9,6 +9,10 @@ rm -rf $EXPNAME-eval
 
 for STEP in {1..7}; do
     ./test-exp.sh $STEP
+    if [[ "$(hostname)" == habakuk && ($STEP == 2 || $STEP == 5) ]]; then
+        echo Waiting for job to end
+        sleep 10
+    fi
 done
 
 ./downward-reports-ipc.py $EXPNAME-eval coverage
