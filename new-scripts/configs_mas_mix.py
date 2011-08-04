a6ff52aa0f1fc305a8f06a50f39f86ae402e32a4
Moritz Gronbach <meismo@arcor.de>
2011-08-04 14:55:11 +0200

    a file containing a few mas-configs

---
+++ new-scripts/configs_mas_mix.py
@@ -0,0 +1,13 @@
+def mas_mix():
+    return [
+        ("mas-1", "--search 'astar(mas(max_states=1,merge_strategy=5,shrink_strategy=shrink_bisimulation(true, false)))'"),
+        ("mas-2", "--search 'astar(mas(max_states=200000,merge_strategy=5,shrink_strategy=shrink_dfp(Enable_Greedy_Bisimulation)))'"),
+        ("mas-3", "--search 'astar(mas(shrink_strategy=shrink_fh(Low, Low)))'"),
+        ("mas-4", "--search 'astar(mas(shrink_strategy=shrink_fh(High, Low)))'"),
+       ("mas-5", "--search 'astar(mas(shrink_strategy=shrink_fh(Low, High)))'"),
+       ("mas-6", "--search 'astar(mas(shrink_strategy=shrink_fh(High, High)))'"),
+        ("mas-7", "--search 'astar(mas(shrink_strategy=shrink_dfp(Default)))'"),
+        ("mas-8", "--search 'astar(mas(shrink_strategy=shrink_bisimulation(true,true)))'"),
+        ("mas-8", "--search 'astar(mas(max_states=1000))'")
+]
+
