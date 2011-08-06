ef951847c2c9a503c7b67f5112bbd178fd3b3f44
Moritz Gronbach <meismo@arcor.de>
2011-08-06 12:46:24 +0200

    included random shrinking in mas-mix-configs

--- new-scripts/configs_mas_mix.py
+++ new-scripts/configs_mas_mix.py
@@ -4,7 +4,7 @@ def mas_mix():
         ("mas-2", "--search 'astar(mas(max_states=200000,merge_strategy=5,shrink_strategy=shrink_dfp(Enable_Greedy_Bisimulation)))'"),
         ("mas-3", "--search 'astar(mas(shrink_strategy=shrink_fh(Low, Low)))'"),
         ("mas-4", "--search 'astar(mas(shrink_strategy=shrink_fh(High, Low)))'"),
-       ("mas-5", "--search 'astar(mas(shrink_strategy=shrink_fh(Low, High)))'"),
+       ("mas-5", "--search 'astar(mas(shrink_strategy=shrink_random()))'"),
        ("mas-6", "--search 'astar(mas(shrink_strategy=shrink_fh(High, High)))'"),
         ("mas-7", "--search 'astar(mas(shrink_strategy=shrink_dfp(Default)))'"),
         ("mas-8", "--search 'astar(mas(shrink_strategy=shrink_bisimulation(false,false)))'"),
