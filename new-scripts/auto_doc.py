d69d73757c85ad93b3e33ac4ee5f461bd7a1ea6f
Moritz Gronbach <meismo@arcor.de>
2011-08-12 17:21:23 +0200

    added a file containing interface to online wiki

--- new-scripts/auto_doc.py
+++ new-scripts/auto_doc.py
@@ -2,9 +2,7 @@
 
 import getpass, subprocess, re
 
-import MoinMoin
-import xmlrpclib
-
+import downward_xmlrpc_test
 import markup
 
 if __name__ == '__main__':
@@ -21,14 +19,6 @@ if __name__ == '__main__':
                        'synergys' : 'LAMAFFSynergy'})
     introductions = dict({'heuristics': "A heuristic specification is either a newly created heuristic instance or a heuristic that has been defined previously. This page describes how one can specify a new heuristic instance. For re-using heuristics, see ReusingHeuristics."})
 
-    name = raw_input("Your wiki username: ")
-    password = getpass.getpass()
-    wikiurl = "http://localhost:8080"
-    homewiki = xmlrpclib.ServerProxy(wikiurl + "?action=xmlrpc2", allow_none=True)
-    #TODO handle invalid password/username
-    auth_token = homewiki.getAuthToken(name, password)
-    mc = xmlrpclib.MultiCall(homewiki)
-    mc.applyAuthToken(auth_token)
     for page in pages:
         title = "AUTODOC"+categories[page[0]]
         text = page[1]
@@ -38,7 +28,6 @@ if __name__ == '__main__':
         doc = markup.Document();
         doc.add_text(text);
         text = doc.render("moin");
-        mc.putPage(title, text)    
-    result = mc()
+        
 
 
