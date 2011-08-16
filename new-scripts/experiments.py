953384a8f611d09d00eebbd5b433df38f037d056
Jendrik Seipp <jendrikseipp@web.de>
2011-08-16 17:17:54 +0200

    add --build-main-script-only option

--- new-scripts/experiments.py
+++ new-scripts/experiments.py
@@ -40,6 +40,8 @@ class ExpArgParser(tools.ArgParser):
         self.add_argument(
             '--shard-size', type=int, default=100,
             help='how many tasks to group into one top-level directory')
+        self.add_argument('--build-main-script-only', action='store_true',
+            help='Only write the main experiment script to disk and exit.')
 
 
 class Experiment(object):
@@ -128,7 +130,11 @@ class Experiment(object):
                               for (var, path) in self.env_vars.items()])
 
         self._set_run_dirs()
+
         self._build_main_script()
+        if self.build_main_script_only:
+            sys.exit()
+
         self._build_resources()
         self._build_runs()
         self._build_properties_file()
@@ -171,8 +177,7 @@ class Experiment(object):
 
     def _set_run_dirs(self):
         """
-        Sets the relative run directories as instance
-        variables for all runs
+        Sets the relative run directories as instance variables for all runs
         """
         def run_number(number):
             return str(number).zfill(5)
@@ -183,12 +188,10 @@ class Experiment(object):
             return 'runs-%s-%s' % (run_number(first_run), run_number(last_run))
 
         current_run = 0
-
         shards = tools.divide_list(self.runs, self.shard_size)
 
         for shard_number, shard in enumerate(shards, start=1):
             shard_dir = os.path.join(self.path, get_shard_dir(shard_number))
-            tools.overwrite_dir(shard_dir)
 
             for run in shard:
                 current_run += 1
