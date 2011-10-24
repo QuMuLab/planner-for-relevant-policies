3b9d0d776ae22e39337e3ef1eec6fc38bea69358
Malte Helmert <helmert@informatik.uni-freiburg.de>
2011-10-24 14:49:58 +0200

    Added experiments and configs for ICAPS 2012 paper bisimulation experiments.

--- new-scripts/mas_configs.py
+++ new-scripts/mas_configs.py
@@ -46,3 +46,64 @@ def configs_mas_ipc_2011():
 
 def configs_mas_all():
     return configs_mas_hhh() + configs_mas_ipc_2011()
+
+def configs_mas_ipc_2011():
+    return [
+        conf_mas("mas-bisim",
+                 """merge_strategy=merge_linear_reverse_level,
+                    shrink_strategy=shrink_bisimulation(
+                        max_states=infinity,
+                        threshold=1,
+                        greedy=true,
+                        initialize_by_h=false,
+                        group_by_h=false)"""),
+        conf_mas("mas-dfp-200k",
+                 """merge_strategy=merge_linear_reverse_level,
+                    shrink_strategy=shrink_bisimulation(
+                        max_states=200000,
+                        threshold=200000,
+                        greedy=false,
+                        initialize_by_h=true,
+                        group_by_h=true)"""),
+        ]
+
+
+def abbrev(word):
+    return "".join(part[0] for part in word.split("_"))
+
+def no_limit_config(reduce_labels, greedy, merge_strategy):
+    name = "%s-%s-%s" % (abbrev(reduce_labels), abbrev(greedy),
+                         abbrev(merge_strategy))
+    mas_args="""
+        reduce_labels=%s,
+        merge_strategy=%s,
+        shrink_strategy=shrink_bisimulation(
+            max_states=infinity,
+            threshold=1,
+            greedy=%s,
+            initialize_by_h=false,
+            group_by_h=false)
+    """ % (reduce_labels, merge_strategy, greedy)
+    config = "--search 'astar(merge_and_shrink(%s))'" % mas_args
+    return conf(name, config)
+
+
+def configs_selectivity_of_bisim():
+    return [no_limit_config(reduce_labels=r,greedy=g,merge_strategy=m)
+            for r in ["false", "true"]
+            for g in ["false", "somewhat", "true"]
+            for m in ["merge_linear_reverse_level", "merge_linear_cg_goal_level"]]
+
+
+def configs_nonplain_bisimulation():
+    return [no_limit_config(reduce_labels=r,greedy=g,merge_strategy=m)
+            for r in ["false", "true"]
+            for g in ["false", "somewhat", "true"]
+            for m in ["merge_linear_reverse_level", "merge_linear_cg_goal_level"]
+            if (r, g, m) != ("false", "false", "merge_linear_reverse_level")]
+
+
+def config_plain_bisimulation():
+    return [no_limit_config(reduce_labels="false",
+                            greedy="false",
+                            merge_strategy="merge_linear_reverse_level")]
