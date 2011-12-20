c09d6299c31d145f69f8da36268d284bc09d74a7
Malte Helmert <helmert@informatik.uni-freiburg.de>
2011-12-20 17:08:16 +0100

    Added experiment script for issue310 and other M&S journal paper scripts.

--- new-scripts/mas_configs.py
+++ new-scripts/mas_configs.py
@@ -71,6 +71,16 @@ def configs_mas_ipc_2011():
 def abbrev(word):
     return "".join(part[0] for part in word.split("_"))
 
+
+def abbrev_num(word):
+    if word == "infinity":
+        return "inf"
+    num = int(word)
+    if num % 1000 == 0:
+        return "%dk" % (num // 1000)
+    return str(num)
+
+
 def no_limit_config(reduce_labels, greedy, merge_strategy):
     name = "%s-%s-%s" % (abbrev(reduce_labels), abbrev(greedy),
                          abbrev(merge_strategy))
@@ -107,3 +117,98 @@ def config_plain_bisimulation():
     return [no_limit_config(reduce_labels="false",
                             greedy="false",
                             merge_strategy="merge_linear_reverse_level")]
+
+
+def configs_mas_ipc_2011():
+    return [
+        conf_mas("mas-bisim",
+                 """merge_strategy=merge_linear_reverse_level,
+                    shrink_strategy=shrink_bisimulation(
+                        max_states=infinity,
+                        threshold=1,
+                        greedy=true,
+                        group_by_h=false)"""),
+        conf_mas("mas-dfp-200k",
+                 """merge_strategy=merge_linear_reverse_level,
+                    shrink_strategy=shrink_bisimulation(
+                        max_states=200000,
+                        threshold=200000,
+                        greedy=false,
+                        initialize_by_h=true,
+                        group_by_h=true)"""),
+        ]
+
+
+def config_jacm(merge_strategy, reduce_labels, greedy, bound, threshold):
+    name = "M%s-R%s-G%s-B%s-T%s" % (
+        abbrev(merge_strategy), abbrev(reduce_labels), abbrev(greedy),
+        abbrev_num(bound), abbrev_num(threshold))
+    mas_args = """
+        reduce_labels=%s,
+        merge_strategy=%s,
+        shrink_strategy=shrink_bisimulation(
+            max_states=%s,
+            threshold=%s,
+            greedy=%s,
+            at_limit=return,
+            group_by_h=false)
+    """ % (reduce_labels, merge_strategy, bound, threshold, greedy)
+    config = "--search 'astar(merge_and_shrink(%s))'" % mas_args
+    return conf(name, config)
+
+
+def configs_issue310():
+    return [
+        config_jacm(merge_strategy=m, reduce_labels=r, greedy=g,
+                    bound=b, threshold=t)
+        for m in ["merge_linear_reverse_level", "merge_linear_cg_goal_level"]
+        for r in ["true"]
+        for g in ["false", "legacy", "true"]
+        for (t, b) in [(1, 100000), (1, "infinity"), (10000, 10000)]
+        ]
+
+
+def configs_jacm_test_label_reduction():
+    return [
+        config_jacm(merge_strategy=m, reduce_labels=r, greedy=g,
+                    bound=b, threshold=t)
+        for m in ["merge_linear_reverse_level", "merge_linear_cg_goal_level"]
+        for r in ["false", "true"]
+        for g in ["false", "true"]
+        for (t, b) in [(1, 100000), (1, "infinity"), (10000, 10000)]
+        ]
+
+
+def configs_jacm_test_label_reduction_setdiff():
+    # We don't include the issue310 configs here since we already computed
+    # those separately.
+    return set_difference(configs_jacm_test_label_reduction(),
+                          configs_issue310())
+
+
+def configs_jacm():
+    return [
+        config_jacm(merge_strategy=m, reduce_labels=r, greedy=g,
+                    bound=b, threshold=t)
+        for m in ["merge_linear_reverse_level", "merge_linear_cg_goal_level"]
+        for r in ["true"]
+        for g in ["false", "true"]
+        for (t, b) in [
+            (1, 10000), (1, 100000), (1, 200000), (1, "infinity"),
+            (10000, 10000), (10000, 100000), (10000, 200000), (10000, "infinity"),
+            (100000, 100000), (100000, 200000), (100000, "infinity"),
+            (200000, 200000), (200000, "infinity"),
+            ("infinity", "infinity"),
+            ]]
+
+
+def configs_jacm_setdiff():
+    # We don't include the issue310 configs here since we already computed
+    # those separately.
+    return set_difference(configs_jacm(), configs_issue310())
+
+
+def set_difference(configs1, configs2):
+    configs2_dict = dict(configs2)
+    return [(name, config) for (name, config) in configs1
+            if name not in configs2_dict]
