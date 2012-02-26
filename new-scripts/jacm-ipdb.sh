c175236620f802d7082bce3165d1e00075040aa2
Malte Helmert <helmert@informatik.uni-freiburg.de>
2012-02-26 16:36:49 +0100

    JACM script update.

--- new-scripts/jacm-ipdb.sh
+++ new-scripts/jacm-ipdb.sh
@@ -8,8 +8,8 @@ SUITE=OPTIMAL_WITH_IPC11
 EXPTYPE=gkigrid
 
 ## For testing, use this:
-SUITE=gripper:prob01.pddl,gripper:prob02.pddl
-EXPTYPE=local
+#SUITE=gripper:prob01.pddl,gripper:prob02.pddl
+#EXPTYPE=local
 
 EXPOPTS=--compact
 export DOWNWARD_TIMEOUT=1800
