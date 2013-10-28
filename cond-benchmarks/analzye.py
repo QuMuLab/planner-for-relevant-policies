45feedde15468ef55b49d4d7efc7d6cc95103e9e
haz <haz@localhost>
2013-10-28 17:53:27 +1100

    Put in overall analysis for the 4 domains tested.

--- cond-benchmarks/analzye.py
+++ cond-benchmarks/analzye.py
@@ -1,10 +1,46 @@
 from krrt.utils import read_file, get_opts
+from krrt.stats.plots import plot, create_time_profile
 
-data = [line.split(',') for line in read_file(get_opts()[0]['-csv'])[1:]]
+good_domains = ['search-and-rescue', 'triangle-tireworld', 'miconic-simpleadl', 'schedule']
+dom_names = ['search', 'tireworld', 'miconic', 'schedule']
 
-times = [float(i[0]) for i in data]
-sizes = [float(i[1]) for i in data]
+if '-csv' in get_opts()[0]:
+    data = [line.split(',') for line in read_file(get_opts()[0]['-csv'])[1:]]
+    
+    times = [float(i[0]) for i in data]
+    sizes = [float(i[1]) for i in data]
+    
+    print "Mean time: %f" % (sum(times) / len(times))
+    print "Mean size: %f" % (sum(sizes) / len(sizes))
 
-print "Mean time: %f" % (sum(times) / len(times))
-print "Mean size: %f" % (sum(sizes) / len(sizes))
+else:
+    times = []
+    sizes = []
+    xs = []
+    for dom in good_domains:
+        data = [line.split(',') for line in read_file("%s.csv" % dom)[1:]]
+        times.append(sorted([float(i[0]) for i in data]))
+        sizes.append(sorted([float(i[1]) for i in data]))
+        xs.append([i+1 for i in range(len(data))])
+
+    plot(x=xs,
+         y=times,
+         x_label="Problem",
+         y_label="Runtime (s)",
+         col=True,
+         y_log=True,
+         #names=dom_names,
+         names=None,
+         xyline=False,
+         no_scatter=True)
+    
+    plot(x=xs,
+        y=sizes,
+        x_label="Problem",
+        y_label="Policy Size",
+        col=True,
+        y_log=False,
+        names=dom_names,
+        xyline=False,
+        no_scatter=True)
 
