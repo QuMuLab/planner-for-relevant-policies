1088876e8fe1e9e763c73cc0ac15c6bba3d6596b
haz <haz@localhost>
2014-07-18 16:45:13 +1000

    Allowed the option for reloading the problem, and started on adding the faults to the encoding.

--- prp-scripts/fault-tolerant-conversion.py
+++ prp-scripts/fault-tolerant-conversion.py
@@ -28,7 +28,7 @@ def print_domain_stats(p):
     print "Actions:\t %d" % len(p.actions)
     print "Max faults:\t %d" % p.max_faults
 
-def convert(dom_name):
+def load_problem(dom_name):
     print "Parsing..."
     p = parser.Problem(dom_name)
     p.max_faults = -1
@@ -38,6 +38,30 @@ def convert(dom_name):
         normalize(a)
 
     print "Ready!"
+
+    return p
+
+def add_fault_limit(p, max_faults):
+
+    p.max_faults = max_faults
+    started = parser.Predicate("started", None, [])
+    p.predicates.append(started)
+    faults = []
+
+    for i in range(max_faults+1):
+        faults.append(parser.Predicate("faults_%d" % i, None, []))
+        p.predicates.append(faults[-1])
+
+    for a in p.actions:
+        assert isinstance(a.precondition, parser.And)
+        a.precondition.args.append(parser.Primitive(started))
+
+    p.actions.append(parser.Action('start-planning', [], None, None, parser.And(map(parser.Primitive, [started, faults[0]]))))
+
+
+def convert(dom_name):
+
+    p = load_problem(dom_name)
 
     while True:
 
@@ -49,7 +73,8 @@ def convert(dom_name):
                                   'Show domain statistics', # 4
                                   'Print the domain', # 5
                                   'Save the domain',  # 6
-                                  'Quit'])            # 7
+                                  'Reload the domain',# 7
+                                  'Quit'])            # 8
 
         if 0 == next_action:
             print
@@ -60,7 +85,17 @@ def convert(dom_name):
         elif 1 == next_action:
             pass
         elif 2 == next_action:
-            pass
+            print
+            if -1 != p.max_faults:
+                print "Error: Max faults already set (reload the domain first)"
+            else:
+                answer = raw_input("Maximum number of faults? ")
+                #try:
+                choice = int(answer)
+                assert choice >= 0
+                add_fault_limit(p, choice)
+                #except:
+                #    print "\nError: You must select a number for max faults (%s)" % str(answer)
         elif 3 == next_action:
             pass
         elif 4 == next_action:
@@ -72,6 +107,8 @@ def convert(dom_name):
         elif 6 == next_action:
             pass
         elif 7 == next_action:
+            p = load_problem(dom_name)
+        elif 8 == next_action:
             print
             return
 
