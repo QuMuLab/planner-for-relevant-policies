57e73a79e882b7ced62ae5eb9a2dbebaba6f4459
haz <haz@localhost>
2015-09-17 18:46:15 +1000

    Experimenting with hyper-aggressive FSAP policies.

--- src/search/planner.cc
+++ src/search/planner.cc
@@ -31,7 +31,7 @@ int main(int argc, const char **argv) {
 
     SearchEngine *engine = 0;
     g_policy = 0;
-    
+
     g_timer_regression.stop();
     g_timer_simulator.stop();
     g_timer_engine_init.stop();
@@ -40,7 +40,7 @@ int main(int argc, const char **argv) {
     g_timer_policy_eval.stop();
     g_timer_policy_use.stop();
     g_timer_jit.stop();
-    
+
     g_timer_regression.reset();
     g_timer_simulator.reset();
     g_timer_engine_init.reset();
@@ -62,7 +62,7 @@ int main(int argc, const char **argv) {
         exit_with(EXIT_INPUT_ERROR);
     }
     g_timer_engine_init.stop();
-    
+
     /* HAZ: Unfortunately, this must go here (as supposed to globals.cc)
      *      since we need to know if g_detect_deadends is true or not. */
     if (g_detect_deadends) {
@@ -73,8 +73,8 @@ int main(int argc, const char **argv) {
     g_deadend_policy = new Policy();
     g_deadend_states = new Policy();
     g_temporary_deadends = new Policy();
-    
-    
+
+
     /***************************************
      * Assert the settings are consistent. *
      ***************************************/
@@ -82,95 +82,118 @@ int main(int argc, const char **argv) {
         ((g_partial_planlocal || g_plan_locally_limited) && !g_plan_locally) ||
         (g_optimized_scd && (g_jic_limit == 0)) ||
         (g_forgetpolicy && (g_jic_limit > 0))) {
-            
+
         cout << "\n  Parameter Error: Make sure that the set of parameters is consistent.\n" << endl;
         exit(0);
     }
-    
-    
-    
+
+
+
     /*********************
      * Handle JIC Limits *
      *********************/
-     
+
     cout << "\nTotal allotted time (s): " << g_jic_limit << endl;
-    
+
     // If we are going to do a final FSAP-free round, then we modify the
     //  time limits to give a 50/50 split between the JIC phase and final
     //  round phase
     double jic_ratio = 0.5;
     if (g_final_fsap_free_round)
         g_jic_limit *= jic_ratio;
-    
+
     cout << "Max time for core JIC (remaining used in final-round repairs): " << g_jic_limit << endl;
-    
+
     // Adjust the g_jic_limit so the epochs are handled properly
     int epochs_remaining = g_num_epochs;
     double single_jic_limit = g_jic_limit / (double)g_num_epochs;
     g_jic_limit = single_jic_limit;
-    
+
     cout << "Max time for each of the " << epochs_remaining << " epochs: " << g_jic_limit << endl << endl;
-    
-    
-    
+
+
+
+
+
+    cout << " !!! Warning !!!" << endl;
+    cout << "  Don't forget to remove the code in ff_heuristic.cc" << endl;
+
+
+
+
     // We start the jit timer here since we should include the initial search / policy construction
     g_timer_jit.resume();
     g_timer_search.resume();
     engine->search();
     g_timer_search.stop();
-    
+
     engine->save_plan_if_necessary();
     engine->statistics();
     engine->heuristic_statistics();
-    
+
     cout << "Initial search time: " << g_timer_search << endl;
     cout << "Initial total time: " << g_timer << endl;
-    
+
     if (!engine->found_solution()) {
         cout << "No solution -- aborting repairs." << endl;
+        cout << "Repeating the search 10 times to see FSAP effectiveness" << endl;
+
+        for (int i=0; i < 10; i++) {
+            cout << "\n ------------------------------" << endl;
+            engine->reset();
+            g_timer_jit.resume();
+            g_timer_search.resume();
+            engine->search();
+            g_timer_search.stop();
+
+            engine->save_plan_if_necessary();
+            engine->statistics();
+            engine->heuristic_statistics();
+        }
+        cout << "\n ------------------------------" << endl;
         exit(1);
     }
-    
+
     cout << "\n\nCreating the simulator..." << endl;
     Simulator *sim = new Simulator(engine, argc, argv, !g_silent_planning);
-    
+
     cout << "\n\nRegressing the plan..." << endl;
     list<PolicyItem *> regression_steps = perform_regression(engine->get_plan(), g_matched_policy, 0, true);
-    
+
     cout << "\n\nGenerating an initial policy..." << endl;
     g_policy = new Policy();
     g_policy->update_policy(regression_steps);
     g_best_policy = g_policy;
     g_best_policy_score = g_policy->get_score();
-    
+
     if (g_sample_for_depth1_deadends)
         sample_for_depth1_deadends(engine->get_plan(), new PartialState(g_initial_state()));
-    
+
     cout << "\n\nComputing just-in-time repairs..." << endl;
     bool changes_made = true;
     while (changes_made) {
         changes_made = perform_jit_repairs(sim);
         if (!g_silent_planning)
             cout << "Finished repair round." << endl;
-        
+
         if (g_timer_jit() >= g_jic_limit) {
             epochs_remaining--;
             if (epochs_remaining > 0)
                 g_jic_limit += single_jic_limit;
         }
-        
+
         // Check if we should re-run the repairs with forbidden ops used
         //  in the heurstic computation.
         if (!changes_made && !g_check_with_forbidden &&
             g_detect_deadends && !(g_policy->is_strong_cyclic()) &&
             (g_timer_jit() < g_jic_limit)) {
-            
+
             g_check_with_forbidden = true;
             changes_made = true;
             if (g_best_policy != g_policy)
                 delete g_policy;
             g_policy = new Policy();
-            
+
             // We need to reset the deadends since they may have been
             //  generated based on faulty heuristic computations that
             //  ignored the forbidden state-action pairs.
@@ -180,7 +203,7 @@ int main(int argc, const char **argv) {
                 delete g_deadend_states;
             g_deadend_policy = new Policy();
             g_deadend_states = new Policy();
-            
+
         }
     }
     if (!g_silent_planning)
@@ -192,41 +215,41 @@ int main(int argc, const char **argv) {
         if (g_best_policy->get_score() > g_policy->get_score())
             g_policy = g_best_policy;
     }
-    
+
     if (g_final_fsap_free_round)
         g_jic_limit /= jic_ratio;
 
     if (!(g_policy->is_strong_cyclic()) && g_final_fsap_free_round) {
-    
+
         bool os1 = g_detect_deadends;
         bool os2 = g_generalize_deadends;
         bool os3 = g_record_online_deadends;
         bool os4 = g_force_limit_states;
         int  os5 = g_limit_states_max;
-        
+
         g_detect_deadends = false;
         g_generalize_deadends = false;
         g_record_online_deadends = false;
         g_force_limit_states = true;
         g_limit_states_max = 1000;
-        
+
         g_policy->return_if_possible = true;
         g_policy->mark_incomplete();
-        
+
         cout << "\n\nDoing one final JIC round ignoring FSAPs for unhandled states." << endl;
         g_timer_jit.resume();
         perform_jit_repairs(sim);
         g_timer_jit.stop();
-        
+
         g_policy->return_if_possible = false;
-        
+
         g_detect_deadends = os1;
         g_generalize_deadends = os2;
         g_record_online_deadends = os3;
         g_force_limit_states = os4;
         g_limit_states_max = os5;
     }
-    
+
     if (g_optimized_scd) {
         cout << "\n\nRunning a final SCD check..." << endl;
         vector< DeadendTuple * > failed_states; // The failed states (used for creating deadends)
@@ -235,50 +258,50 @@ int main(int argc, const char **argv) {
         while (made_change)
             made_change = g_policy->step_scd(failed_states, false);
     }
-    
+
     // Allow selection of a forbidden action when no legal actions exist
     g_policy->return_if_possible = true;
 
     if (g_optimize_final_policy) {
         g_timer_policy_build.resume();
-        
+
         cout << "\n\nRunning a final JIT repair to see which pairs are relevant..." << endl;
         g_record_relevant_pairs = true;
         perform_jit_repairs(sim);
         g_record_relevant_pairs = false;
         cout << "...and creating the new policies..." << endl;
-        
+
         g_timer_policy_build.stop();
-        
+
         // Rebuild the policies
         Policy * newPol = new Policy();
         Policy * newFSAP = new Policy();
-        
+
         list<PolicyItem *> reg_items;
         list<PolicyItem *> fsap_items;
-        
+
         g_policy->copy_relevant_items(reg_items, true);
         g_deadend_policy->copy_relevant_items(fsap_items);
-        
+
         newPol->update_policy(reg_items);
         newFSAP->update_policy(fsap_items);
-        
+
         // Re-enable the return_if_possible and other settings
         newPol->return_if_possible = true;
         if (g_policy->is_complete())
             newPol->mark_complete();
         if (g_policy->is_strong_cyclic())
             newPol->mark_strong();
-        
+
         // DO NOT DELETE: It deletes every PolicyItem
         //delete g_policy;
         //delete g_deadend_policy;
-        
+
         g_policy = newPol;
         g_deadend_policy = newFSAP;
-        
+
     }
-    
+
     // Reset the deadend and scd settings for the online simulation(s)
     g_detect_deadends = false;
     g_generalize_deadends = false;
@@ -289,37 +312,37 @@ int main(int argc, const char **argv) {
     g_timer_simulator.resume();
     sim->run();
     g_timer_simulator.stop();
-    
-    
+
+
     g_timer.stop();
-    
-    
+
+
     cout << "\n\n" << endl;
     sim->dump();
     cout << "\n\n" << endl;
-    
+
     if (1 == g_dump_policy) {
-        
+
         cout << "Dumping the policy and fsaps..." << endl;
         ofstream outfile;
-        
+
         outfile.open("policy.out", ios::out);
         g_policy->generate_cpp_input(outfile);
         outfile.close();
-        
+
         outfile.open("policy.fsap", ios::out);
         g_deadend_policy->generate_cpp_input(outfile);
         outfile.close();
-        
+
     } else if (2 == g_dump_policy) {
         cout << "Dumping the policy and fsaps..." << endl;
         g_policy->dump_human_policy();
         g_deadend_policy->dump_human_policy(true);
     }
-    
+
     if (g_policy->is_strong_cyclic())
         exit_with(EXIT_STRONG_CYCLIC);
     else
         exit_with(EXIT_NOT_STRONG_CYCLIC);
-    
+
 }
--- src/search/policy-repair/deadend.cc
+++ src/search/policy-repair/deadend.cc
@@ -2,7 +2,7 @@
 
 
 bool is_deadend(PartialState &state) {
-    
+
     // We must always set up the forbidden operators. Why? Well when we
     //  aren't checking with forbidden operators, they are always removed
     //  from the relaxed reachability -- this may create incorrect deadends
@@ -17,73 +17,73 @@ bool is_deadend(PartialState &state) {
 
 
 bool generalize_deadend(PartialState &state) {
-    
+
     bool debug = false;
-    
+
     // If the whole state isn't recognized as a deadend, then don't bother
     //  looking for a subset of the state
     if (!is_deadend(state))
         return false;
-    
+
     // We go through each variable and unset it, checking if the relaxed reachability
     //  is violated.
     for (int i = 0; i < g_variable_name.size(); i++) {
-        
+
         int old_val = state[i];
         state[i] = -1;
-        
+
         // If relaxing variable i causes us to reach the goal, keep it in
         if (!is_deadend(state))
             state[i] = old_val;
     }
-    
+
     if (debug) {
         cout << "Found relaxed deadend:" << endl;
         state.dump_pddl();
     }
-    
+
     return true;
 }
 
 void update_deadends(vector< DeadendTuple* > &failed_states) {
-    
+
     list<PolicyItem *> de_items;
     list<PolicyItem *> de_states;
-    
+
     bool debug = false;
-    
+
     PartialState * dummy_state = new PartialState();
-    
+
     for (int i = 0; i < failed_states.size(); i++) {
-        
+
         // Generalize the deadend if need be
         PartialState * failed_state = failed_states[i]->de_state;
         PartialState * failed_state_prev = failed_states[i]->prev_state;
         const Operator * prev_op = failed_states[i]->prev_op;
-        
+
         if (debug) {
             cout << "\n(#" << g_debug_count++ << ") Creating forbidden state-action pairs for deadend:" << endl;
             failed_state->dump_pddl();
             cout << endl;
         }
-        
+
         // Add the failed state to our list of deadends
         de_states.push_back(new NondetDeadend(new PartialState(*failed_state)));
-        
+
         // HAZ: Only do the forbidden state-action computation when
         //  the non-deterministic action doesn't have any associated
         //  conditional effects. This is ensured by the construction
         //  of the g_regressable_ops data structure.
-        
+
         // Get the regressable operators for the given state.
         vector<PolicyItem *> reg_items;
         g_regressable_ops->generate_applicable_items(*failed_state, reg_items, true);
-        
+
         // For each operator, create a new deadend avoidance pair
         for (int j = 0; j < reg_items.size(); j++) {
-            
+
             RegressableOperator *ro = (RegressableOperator*)(reg_items[j]);
-            
+
             de_items.push_back(new NondetDeadend(new PartialState(*failed_state, *(ro->op), false, dummy_state),
                                                      ro->op->nondet_index));
             if (debug) {
@@ -91,18 +91,18 @@ void update_deadends(vector< DeadendTuple* > &failed_states) {
                 de_items.back()->dump();
             }
         }
-        
+
         ////////////////////////////////////////////
-        
+
         // Check to see if we have any consistent "all-fire" operators
         reg_items.clear();
         g_regressable_cond_ops->generate_applicable_items(*failed_state, reg_items, true);
-        
+
         // For each operator, create a new deadend avoidance pair
         for (int j = 0; j < reg_items.size(); j++) {
-            
+
             RegressableOperator *ro = (RegressableOperator*)(reg_items[j]);
-            
+
             de_items.push_back(new NondetDeadend(
                                     new PartialState(*failed_state, *(ro->op), false, ro->op->all_fire_context),
                                     ro->op->nondet_index));
@@ -112,41 +112,41 @@ void update_deadends(vector< DeadendTuple* > &failed_states) {
                 de_items.back()->dump();
             }
         }
-        
+
         ////////////////////////////////////////////
-        
+
         // If we have a specified previous state and action, use that to
         //  build a forbidden state-action pair
         if (NULL != failed_state_prev) {
             de_items.push_back(new NondetDeadend(
                     new PartialState(*failed_state, *prev_op, false, failed_state_prev),
                     prev_op->nondet_index));
-            
+
             if (debug) {
                 cout << "Creating new (default) forbidden state-action pair:" << endl;
                 de_items.back()->dump();
             }
         }
     }
-    
+
     delete dummy_state;
-    
+
     g_deadend_policy->update_policy(de_items);
     g_deadend_states->update_policy(de_states);
-    
+
     if (g_repeat_fsap_backwards) {
         for (std::list<PolicyItem *>::iterator it=de_items.begin(); it != de_items.end(); ++it) {
-            
+
             // Make sure the partial state isn't already a deadend
             if (!(g_deadend_states->check_match(*((*it)->state), false))) {
-                
+
                 // Just call the successor generator to see if the combination is triggered
                 vector<const Operator *> ops;
                 g_successor_generator->generate_applicable_ops(*((*it)->state), ops);
                 if (ops.size() == 0)
                     g_repeat_fsap_count++;
             }
-            
+
         }
     }
 }
@@ -154,30 +154,30 @@ void update_deadends(vector< DeadendTuple* > &failed_states) {
 
 void DeadendAwareSuccessorGenerator::generate_applicable_ops(const StateInterface &_curr, vector<const Operator *> &ops) {
     if (g_detect_deadends && g_deadend_policy) {
-        
+
         PartialState curr = PartialState(_curr);
-        
-        bool debug = false;
-        
+
+        bool debug = true;
+
         vector<PolicyItem *> reg_items;
         vector<const Operator *> orig_ops;
         map<int, PolicyItem *> fsap_map;
-        
+
         g_successor_generator_orig->generate_applicable_ops(_curr, orig_ops, true);
         g_deadend_policy->generate_applicable_items(curr, reg_items);
-        
+
         set<int> forbidden;
         for (int i = 0; i < reg_items.size(); i++) {
-            
+
             int index = ((NondetDeadend*)(reg_items[i]))->op_index;
-            
+
             forbidden.insert(index);
-            
+
             if ((fsap_map.find(index) == fsap_map.end()) ||
                 (reg_items[i]->state->size() < fsap_map[index]->state->size()))
                     fsap_map[index] = reg_items[i];
         }
-        
+
         vector<int> ruled_out;
         for (int i = 0; i < orig_ops.size(); i++) {
             if (0 == forbidden.count(orig_ops[i]->nondet_index)) {
@@ -186,10 +186,10 @@ void DeadendAwareSuccessorGenerator::generate_applicable_ops(const StateInterfac
                 ops.push_back(orig_ops[i]);
             }
             else {
-                
+
                 if (g_combine_deadends)
                     ruled_out.push_back(orig_ops[i]->nondet_index);
-                
+
                 /* HAZ: Since we are using the ff heuristic with preferred operators,
                  *      we don't want the preferred operators to sneak into the applicable
                  *      list of actions. As such, we "mark" the operator which prevents
@@ -197,54 +197,54 @@ void DeadendAwareSuccessorGenerator::generate_applicable_ops(const StateInterfac
                  *      for where this occurrs.
                  */
                 orig_ops[i]->mark();
-                
+
                 if (debug)
                     cout << "Forbidding operator " << orig_ops[i]->get_name() << endl;
             }
         }
-        
+
         // Add this state as a deadend if we have ruled out everything
         if (!g_limit_states && g_record_online_deadends &&
              g_combine_deadends && (orig_ops.size() > 0) && ops.empty()) {
-                 
+
             PartialState *newDE = new PartialState();
             for (int i = 0; i < ruled_out.size(); i++) {
                 newDE->combine_with(*(((NondetDeadend*)(fsap_map[ruled_out[i]]))->state));
             }
-            
+
             if (debug) {
                 cout << "<< (#" << g_debug_count++ << ") Found a new deadend state of size " << newDE->size() << " >>" << endl;
                 newDE->dump_pddl();
             }
-            
+
             g_combined_count++;
-            
+
             vector<DeadendTuple *> failed_states;
             failed_states.push_back(new DeadendTuple(newDE, NULL, NULL));
             g_updated_deadends = true;
             update_deadends(failed_states);
         }
-        
+
     } else {
-        
+
         g_successor_generator_orig->generate_applicable_ops(_curr, ops, true);
-        
+
     }
     return;
 }
 
 
 bool sample_for_depth1_deadends(const SearchEngine::Plan &plan, PartialState *state) {
-    
+
     bool debug = false;
-    
+
     if (!g_detect_deadends)
         return false;
-    
+
     vector<DeadendTuple *> new_deadends;
     PartialState* old_s = state;
     PartialState* new_s;
-    
+
     for (int i = 0; i < plan.size(); i++) {
         for (int j = 0; j < g_nondet_mapping[plan[i]->nondet_index]->size(); j++) {
             PartialState *succ_state = new PartialState(*old_s, *((*(g_nondet_mapping[plan[i]->nondet_index]))[j]));
@@ -254,12 +254,12 @@ bool sample_for_depth1_deadends(const SearchEngine::Plan &plan, PartialState *st
                 new_deadends.push_back(new DeadendTuple(succ_state, new PartialState(*old_s), (*(g_nondet_mapping[plan[i]->nondet_index]))[j]));
             }
         }
-        
+
         new_s = new PartialState(*old_s, *plan[i]);
         delete old_s;
         old_s = new_s;
     }
-    
+
     if (new_deadends.size() > 0) {
         if (debug)
             cout << "Found " << new_deadends.size() << " new deadends!" << endl;
@@ -267,7 +267,7 @@ bool sample_for_depth1_deadends(const SearchEngine::Plan &plan, PartialState *st
         update_deadends(new_deadends);
         return true;
     }
-    
+
     return false;
 }
 
