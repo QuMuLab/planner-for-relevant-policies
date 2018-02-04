878e873a33295d5c013fc5d30840de4e45a94731
Christian Muise <christian.muise@gmail.com>
2018-02-03 21:12:58 -0500

    Fixing a bug with policy parsing empty conditions.

--- prp-scripts/validators/prp.py
+++ prp-scripts/validators/prp.py
@@ -30,16 +30,16 @@ def load(pol, fmap):
 
     POLICY = []
     FSAP = {}
-    
+
     STAGE_MAP = 0
     STAGE_POL = 1
     STAGE_FSAP = 2
-    
+
     stage = STAGE_MAP
 
     while file_lines:
         fluent_line = file_lines.pop(0)
-        
+
         if fluent_line == 'Policy:':
             stage = STAGE_POL
             continue
@@ -48,15 +48,15 @@ def load(pol, fmap):
             continue
         elif stage == STAGE_MAP:
             continue
-        
+
         nfluents = set([fmap[f.strip().replace(',', '')[4:-1]] for f in \
                         filter(lambda x: 'not(' == x[:4], \
                                fluent_line.split(':')[-1][1:].split('/'))])
         pfluents = set([fmap[f.strip().replace(',', '')] for f in \
-                        filter(lambda x: 'not(' != x[:4], \
+                        filter(lambda x: ('not(' != x[:4]) and (len(x) > 0), \
                                fluent_line.split(':')[-1][1:].split('/'))])
         action = file_lines.pop(0).split(':')[-1].split('/')[0][1:-1].strip().replace(' ', '_')
-        
+
         if STAGE_POL == stage:
             POLICY.append((nfluents, pfluents, action))
         elif STAGE_FSAP == stage:
