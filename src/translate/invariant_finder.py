1e3a6447c1db80d0ef9e5c0137ca24d97baecb21
Christian Muise <christian.muise@gmail.com>
2022-10-16 22:05:21 -0400

    Updating the old invariant code.

--- src/translate/invariant_finder.py
+++ src/translate/invariant_finder.py
@@ -99,10 +99,10 @@ def find_invariants(task, reachable_action_params):
             candidates.append(invariant)
             seen_candidates.add(invariant)
 
-    start_time = time.clock()
+    start_time = time.time()
     while candidates:
         candidate = candidates.popleft()
-        if time.clock() - start_time > MAX_TIME:
+        if time.time() - start_time > MAX_TIME:
             print("Time limit reached, aborting invariant generation")
             return
         if candidate.check_balance(balance_checker, enqueue_func):
